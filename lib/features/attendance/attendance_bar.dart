d0676f8a6b8dad9756c768be740e6d10e24c55b6
rohit0110 <rohit7rathore15@gmail.com>
2022-02-24 22:34:43 +0530

    Created Basic Attendance Page

---
+++ lib/features/attendance/attendance_bar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+//Widget containing List of Subjects and their respective attendances
+class AttendanceBar extends StatefulWidget {
+  const AttendanceBar({Key? key}) : super(key: key);
+
+  @override
+  _AttendanceBarState createState() => _AttendanceBarState();
+}
+
+class _AttendanceBarState extends State<AttendanceBar> {
+  //list that will contain widgets with attendance information
+  final attendanceList = [
+    "Attendance 1",
+    "Attendance 2",
+    "Attendance 3",
+    "Attendance 4"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+          itemCount: attendanceList.length,
+          itemBuilder: (context, index) {
+            return Card(
+                color: Colors.amber,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: ListTile(
+                  title: Text(attendanceList[index]),
+                ));
+          }),
+    );
+  }
+}
